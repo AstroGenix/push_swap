bf9546f461e7be7cce3f552d8feb6bead7b7b706
Denys Berehovyy <email4stuff1need@gmail.com>
2023-03-21 23:07:53 +0000

    First commit - adding push_swap

---
+++ includes/push_swap.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dberehov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 11:19:57 by dberehov          #+#    #+#             */
+/*   Updated: 2023/03/14 11:20:02 by dberehov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+// C Libraries
+# include <unistd.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+// Stack Structure
+typedef struct s_list
+{
+	int				num;
+	struct s_list	*next;	
+}	t_list;
+
+
+// Main Functions
+
+#endif
---
+++ main.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dberehov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 11:12:57 by dberehov          #+#    #+#             */
+/*   Updated: 2023/03/14 11:12:59 by dberehov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Create special cases for:
+• When there is a low ammount of numbers;
+*/
+
+#include "includes/push_swap.h"
+
+int	main(int argc, char const *argv[])
+{
+	t_list	**stack_a;
+	t_list	**stack_b;
+	int		i;
+
+	i = 0;
+	if (argc < 2)
+		return (0);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	stack_b = (t_list **)malloc(sizeof(t_list));
+	stack_a = NULL;
+	stack_b = NULL;
+	list_init(stack_a, argc, argv);
+	printf("Stack A\t|\tStack B\n");
+	while (argv[i])
+    {
+		printf("%s\t|\t%s\n", argv[i]);
+		i++;
+    }
+	free(stack_a);
+	free(stack_b);
+
+	return (1);
+}
+
+static void	list_init(t_list **stack_a, int argc, char *argv[])
+{
+	t_list	*new;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+
+	}
+}
+
+// LIBFT LIBRARIES
+
+t_list addnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
