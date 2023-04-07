852fdd630d0a02528d070cf4fe6039494854d3b1
Denys Berehovyy <email4stuff1need@gmail.com>
2023-04-07 22:28:04 +0100

    Dup check, sign check

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ Test for github
 
 #include "includes/push_swap.h"
 
-int	main(int argc, char const *argv[])
+int	main(int argc, char *argv[])
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
@@ -26,41 +26,92 @@ int	main(int argc, char const *argv[])
 	if (argc < 2) // If /a.out is the only input exit.
 		return (0);
 	if (check_input(argv) == false) // Check input for abnormalities or duplicates (unique numbers only).
-		return (0);
+		printf("Something went wrong!");//return (0);
+	else
+		printf("Sweet!");
 
 	return (0);
 }
 
-bool	check_input(char *argv[]) // main - checks input for incorrect values (accepted values are unique positive or negative numbers)
+bool	check_input(char *argv[]) // main - checks input for incorrect values (accepted values are unique positive or negative numbers).
 {
-	int i;
-	int	errors;
+	int	i;
 
 	i = 1;
-	errors = 0;
-	while (argv[i])
+	while (argv[i]) // Loop to check for not numbers.
 	{
-		if (!check_number(argv[i]))
-			errors++;
+		if (check_number(argv[i]) == false) // If return is false -> argv[i] wasn't a number.
+			return (false);
 		i++;
 	}
-	if (errors == 0 && check_duplicates(argv) == 0)
-		return (true);
-	else
+	if (check_duplicates(argv) == true) // Checks for duplicates.
 		return (false);
+	return (true);
 }
 
-bool	check_number(char *argv[])
+bool	check_number(char *argv) // check_input - Checks if input contains only a number.
 {
+	int	i;
 
+	i = 0;
+	if ((argv[i] == '+' || argv[i] == '-') && argv[i + 1] != '\0') // Skips positive and negative and check if number follows sign.
+		i++;
+	while (argv[i])
+	{
+		if ('0' <= argv[i] && argv[i] <= '9')
+			i++;
+		else
+			return (false);
+	}
+	return (true);
 }
 
-int	check_duplicates(char *argv[]) // check_input - verifies the input for duplicate values.
+bool	check_duplicates(char *argv[]) // check_input - verifies the input for duplicate values.
 {
+	int	i;
+	int	j;
 
+	i = 1;
+	while(argv[i])
+	{
+		j = 1;
+		while (argv[j])
+		{
+			if (j != i && dupstring_compare(argv[i],argv[j]) == 0)
+				return (true);
+			j++;
+		}
+		i++;
+	}
+	return (false);
 }
 
-static void	list_init(t_list **stack_a, int argc, char *argv[])
+int	dupstring_compare(const char *str1, const char *str2) // check_duplicates - Compares two strings of digits to check if they are the same or not.
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (str1[i] == '+') //  +1 == 1 but -1 != 1.
+	{
+		if (str2[j] != '+') // Skips '+' sign if false.
+		i++;
+	}
+	else
+	{
+		if (str2[j] == '+')
+		i++;
+	}
+	while(str1[i] && str2[j] && str1[i] == str2[j])
+	{
+		i++;
+		j++;
+	}
+	return ((unsigned char)str1[i] - (unsigned char)str2[j]); // Return 0 if the strings match.
+}
+
+/*static void	list_init(t_list **stack_a, int argc, char *argv[])
 {
 	t_list	*new;
 	int		i;
@@ -91,3 +142,4 @@ int list_newnode(int *num)
 	new_node->next = NULL;
 	return (new_node);
 }
+*/
