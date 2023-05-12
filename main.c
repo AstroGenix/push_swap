2c65d65b34fda2783f01c7b6b97f7438eaf12fd1
AstroGenix <email4stuff1need@gmail.com>
2023-05-12 12:15:23 +0100

    Finished

--- main.c
+++ main.c
@@ -31,10 +31,9 @@ int	main(int argc, char *argv[])
 
 	if (argc < 2) // If /a.out is the only input exit.
 		return (0);
-	else if (argc == 2)
-		split = ft_split(argv[1], ' ');
-	else
+	else if (argc > 2)
 		error(NULL, NULL);
+	split = ft_split(argv[1], ' ');
 	if (check_input(split) == false) // Check input for abnormalities or duplicates (unique numbers only).
 		error(NULL, NULL);
 	len = arr_size(split);
@@ -79,39 +78,3 @@ void	rank_values(t_stack *a, int stack_size)
 			top->rank = stack_size;
 	}
 }
-
-/*
-int	main(int argc, char *argv[])
-{
-	t_stack	*a;
-	t_stack	*b;
-	int		stack_size;
-
-	if (argc < 2) // If /a.out is the only input exit.
-		return (0);
-	if (check_input(argv) == false) // Check input for abnormalities or duplicates (unique numbers only).
-		error(NULL, NULL);
-	b = NULL;
-	a = insert_values(argc, argv);
-	stack_size = fetch_stack_size(a);
-	rank_values(a, stack_size + 1);
-	if (!is_sorted(a))
-		which_sort(&a,&b,stack_size);
-	// Free both stacks.
-	free_stack(&a);
-	free_stack(&b);
-	return (0);
-}
-The part that works is like this.
-./push_swap 3 2 1 -> works as intended, argv[0] = './push_swap' argv[1] = 3
-This is what my code was doing and it was working still is. However problem arrives if I have
-to do something like ./push_swap "3 2 1" which should also work however it goes through a split func
-that turns it into argv[0] = 3 argv[1] = 2.
-And so there is a missmatch.
-
-./push_swap 3 2 1     | ./push_swap "3 2 1"
-argv[0]='./push_swap' | goes into ft_split
-argv[1]='3'           |argv[0]='3'
-argv[2]='2'           |argv[1]='2'
-argv[3]='1'           |argv[2]='1'
-*/
